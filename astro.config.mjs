a295ff5228bbe39ea31fe94b056ca0b96810f6c1
wcgordon1 <wcgordon1@gmail.com>
2024-12-17 05:59:12 -0800

    sponsorship card and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    // Disable the default base styles
-    applyBaseStyles: false,
-  })],
+  site: 'https://indianapropertytaxcalculator.com',
+  integrations: [
+    tailwind({
+      // Disable the default base styles
+      applyBaseStyles: false,
+    }),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+      filter: (page) => !page.includes('private'),
+    }),
+  ],
 });
